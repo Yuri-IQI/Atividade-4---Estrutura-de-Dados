629ac273292d2b683f78b48f97c0af18d9d19509
Yuri-IQI <anthony.yuri.ff@gmail.com>
2025-05-26 21:35:28 -0300

    no ar?

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Atividade-4---Estrutura-de-Dados/',
+  base: './',
+  build: {
+    outDir: 'dist',
+  }
 })
